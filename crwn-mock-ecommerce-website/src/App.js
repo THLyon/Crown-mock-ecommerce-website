e5db1603cf237614e709a1efa1ce17c67d3ba2c9
Tanner Lyon <tannerhlyon@gmail.com>
2023-02-03 21:37:10 -0500

    updated app and directory

--- crwn-mock-ecommerce-website/src/App.js
+++ crwn-mock-ecommerce-website/src/App.js
@@ -1,5 +1,5 @@
 import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
 
@@ -33,10 +33,8 @@ const App = () => {
   
 
   return (
-    <div className='categories-container'>
-    {categories.map((category) => (
-      <CategoryItem key={category.id} category={category}/>
-    ))}
+    <div>
+      <Directory categories={categories}/>
     </div>
   );
 }
