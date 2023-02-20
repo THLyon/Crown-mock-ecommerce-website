55b6b7e966e8c6d8ecf207749ff7500fd1ee6d5d
Tanner <tannerhlyon@gmail.com>
2023-02-20 10:45:13 -0800

    added product context

---
+++ lyon-mock-ecommerce-website/src/routes/shop/shop.component.jsx
@@ -0,0 +1,18 @@
+import SHOP_DATA from '../../shopData.json'; 
+import { useContext } from 'react'; 
+import { ProductsContext } from '../../contexts/products.context';
+
+const Shop = () => {
+    const {products} = useContext(ProductsContext)
+    return (
+        <div>
+            {SHOP_DATA.map(({id, name})=> (
+                <div key = {id}> 
+                    <h1> {name} </h1>
+                </div>
+            ))}
+        </div>
+    )
+}; 
+
+export default Shop; 
