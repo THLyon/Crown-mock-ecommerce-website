b49ec174db087577d432af84054f306bc1f611c2
Tanner <tannerhlyon@gmail.com>
2023-02-21 13:22:10 -0800

    typescript

---
+++ lyon-mock-ecommerce-website/src/types/test.tsx
@@ -0,0 +1,19 @@
+
+export type ServerError = {
+    log: string,
+    status?: number,
+    message: { err: string }
+  }
+  
+  export type Scoreboard = {
+    X: number,
+    O: number
+  }
+  
+  export type Player = 'X' | 'O';
+  
+  export type BoardText = '-' | 'X' | 'O';
+  
+  export type BoardContent = Array<BoardText>[];
+  
+  
