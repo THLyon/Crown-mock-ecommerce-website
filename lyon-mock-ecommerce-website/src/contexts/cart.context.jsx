7cb9fe39dbc8871f69a9d83b45ba80f1b53c22a8
Tanner <tannerhlyon@gmail.com>
2023-02-26 10:40:36 -0800

    created full functioning checkoutpage, checkout Items, checkout cart functionality

--- lyon-mock-ecommerce-website/src/contexts/cart.context.jsx
+++ lyon-mock-ecommerce-website/src/contexts/cart.context.jsx
@@ -27,16 +27,21 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
         ? {...cartItem, quantity: cartItem.quantity - 1}
         : cartItem
         );
-    };
+};
 
+const clearCartItem = (cartItems, cartItemToClear) => {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id); 
+};
 
 export const CartContext = createContext({
     isCartOpen: false, 
     setIsCartOpen: () => {},
     cartItems: [], 
     addItemToCart: () => {},
-    removeItermFromCart: () => {},
+    removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0, 
+    cartTotal: 0, 
 }); 
 
 
@@ -45,10 +50,16 @@ export const CartProvider = ({children} ) => {
     const [isCartOpen, setIsCartOpen] = useState(false); 
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);  
+    const [cartTotal, setCartTotal] = useState(0); 
 
     useEffect(() => {
         const newCartCount = cartItems.reduce((total, cartItem )=> total + cartItem.quantity, 0); 
         setCartCount(newCartCount)
+    }, [cartItems])
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem )=> total + cartItem.quantity * cartItem.price, 0); 
+        setCartTotal(newCartTotal)
     }, [cartItems])
 
     const addItemToCart = (productToAdd) => {
@@ -59,7 +70,11 @@ export const CartProvider = ({children} ) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove)); 
     };
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemToCart};
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear)); 
+    };
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemToCart, clearItemFromCart, cartTotal};
 
     return <CartContext.Provider value={value}> {children} </CartContext.Provider>;
 };
--- lyon-mock-ecommerce-website/src/routes/checkout/checkout.component.jsx
+++ lyon-mock-ecommerce-website/src/routes/checkout/checkout.component.jsx
@@ -4,7 +4,7 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import './checkout.styles.scss'; 
 
 const Checkout = () => {
-    const {cartItems, addItemToCart, removeItemToCart} = useContext(CartContext); 
+    const {cartItems, cartTotal} = useContext(CartContext); 
     return(
         <div className='checkout-container'>
             <div className='checkout-header'>
@@ -25,12 +25,11 @@ const Checkout = () => {
                 </div>
             </div>
                 {cartItems.map((cartItem) => {
-                        const {id, name, quantity} = cartItem; 
                     return (
                         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
                     )
                 })}
-            <span className='Total'> Total: 0</span>
+            <span className='total'> Total: ${cartTotal} </span>
         </div>
     )
 };
